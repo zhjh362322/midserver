a17f73c8660b29d10ecae13a40d8818c85c0c39e
baojian <252532242@qq.com>
2018-06-22 10:00:50 +0800

    下单和线路报价调整

--- routes/consignment.js
+++ routes/consignment.js
@@ -2,9 +2,24 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.post('/', function(req, res, next) {
+router.route('/').get(function(req, res) {
+    var q = req.query;
+    var from = q.from;
+    var uid = q.uid;
+    var host = 'http://zgt.zwh56.com';
+    var path = '/mini/consignment';
+    request.get({url: host + path, json: true, qs: {from: from, uid: uid}}, function(error, response, body) {
+        var rstJson = {};
+        if(error) {
+            rstJson.code = 0;
+            rstJson.msg = '网络错误';
+        } else {
+            rstJson = body;
+        }
+        res.send(rstJson);
+    })
+}).post(function(req, res, next) {
     var data = req.body;
-    console.log(data)
     request.post({
         url: 'http://zgt.zwh56.com/mini/consignment',
         method: 'POST',
--- routes/quotation.js
+++ routes/quotation.js
@@ -11,11 +11,10 @@ router.get('/', function(req, res, next) {
     var host = 'http://zgt.zwh56.com';
     var path = '/mini/quotation';
     // var param = '?from=' + from + '&startCity=' + startCity + '&endCity=' + endCity;
-    var qs = {from: 'mini', startCity: startCity, endCity: endCity};
+    var qs = {from: from, startCity: startCity, endCity: endCity};
     request.get({url: host + path, qs: qs, json: true}, function(error, response, body) {
         var rstJson = {};
         if(error) {
-            console.log(error)
             rstJson.code = 0;
             rstJson.msg = '网络错误';
         } else {
